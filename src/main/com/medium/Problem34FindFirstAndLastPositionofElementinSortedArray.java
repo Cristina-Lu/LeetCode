544dfa4800b8b9dee0cd1815f8cae84d526888af
Cartier Qian <luhuiying1024@163.com>
2021-02-19 15:26:50 +0800

    problem 34 二分查找

--- src/main/com/medium/Problem34FindFirstAndLastPositionofElementinSortedArray.java
+++ src/main/com/medium/Problem34FindFirstAndLastPositionofElementinSortedArray.java
@@ -14,25 +14,87 @@ public class Problem34FindFirstAndLastPositionofElementinSortedArray {
     * 二分法找到target，然后找到在该位置前找到第一个小于target的值，
     * 在该位置后找到第一个大于target的值
     * */
-    public int[] searchRange(int[] nums, int target) {
-        int[] result = new int[2];
-        int length = nums.length;
-        int left = nums[0],right = nums[length-1],tag = length/2;
-
+    public int[] searchRange(int[] nums, int target){
+        int left = 0,right = nums.length-1,tag = -1;
+        int[] result = {-1,-1};
         //二分法找到target
-        while (nums[tag] != target){
-            if(nums[tag] > target){
-                right = tag;
-            }else {
-                left = tag;
+        while (left<=right){
+            tag = (left+right)/2;
+            if(nums[tag] == target){
+                break;
             }
-            tag = (right + left)/2;
+            if(nums[tag] > target){
+                right = tag-1;
+            }else {
+                left = tag+1;
+            }
+        }
+        if(left > right){
+            return result;
         }
 
+        left = tag;
+        while (left>=0&&!(nums[left]<target)){
+            --left;
+        }
+        right = tag;
+        while (right<nums.length&&!(nums[right]>target)){
+            ++right;
+        }
+        result[0]=left+1;
+        result[1]=right-1;
+        return result;
+
+    }
+    public int[] searchRange1(int[] nums, int target) {
+        int[] result = new int[2];
+        int length = nums.length;
+        int left = 0,right = length-1,tag = -1;
+
+        //二分法找到target
+        while (left<=right){
+            tag = (left+right)/2;
+            if(nums[tag] == target){
+                break;
+            }
+            if(nums[tag] > target){
+                right = tag-1;
+            }else {
+                left = tag+1;
+            }
+        }
+        if(left > right)
+            return new int[]{-1, -1};
         /*
         * 二分法找到边界
         * 思路，tag≠target，tag+1或者tag-1 = target
         * */
+
+        //找左边界
+        int leftTag = (tag+left)/2,rTag = tag;
+        while (!((leftTag ==0 && nums[leftTag]==target)||(nums[leftTag] == target && nums[leftTag-1] < target ))){
+            if(nums[leftTag] >= target){
+                rTag = leftTag-1;
+            }else {
+                left = leftTag+1;
+            }
+            leftTag = (rTag + left)/2;
+        }
+
+        //二分法找右边界
+        rTag = tag;
+        int rightTag = (rTag + right)/2;
+        while (!((rightTag ==nums.length-1 && nums[rightTag]==target)||(nums[rightTag]==target && nums[rightTag+1] >target) )){
+            if(nums[rightTag] <= target){
+                rTag = rightTag+1;
+            }else {
+                right = rightTag-1;
+            }
+            rightTag = (rTag + right)/2;
+        }
+
+        result[0] = leftTag;
+        result[1] = rightTag;
 
         return result;
     }
--- src/test/com/medium/Problem34FindFirstAndLastPositionofElementinSortedArrayTest.java
+++ src/test/com/medium/Problem34FindFirstAndLastPositionofElementinSortedArrayTest.java
@@ -8,9 +8,35 @@ public class Problem34FindFirstAndLastPositionofElementinSortedArrayTest {
         Problem34FindFirstAndLastPositionofElementinSortedArray problem34FindFirstAndLastPositionofElementinSortedArray
                 = new Problem34FindFirstAndLastPositionofElementinSortedArray();
 
-        int[] nums = {1,2,3,3,4,5,5,5,5,6,7,7,7,7};
-        //当tag非最前也非最后时
-        int[] result = problem34FindFirstAndLastPositionofElementinSortedArray.searchRange(nums,3);
+        int[] nums = {1,1,2,3,3,4,5,5,5,5,7,7,7,7};
+        //当target非最前也非最后且有重复值
+        int[] result = problem34FindFirstAndLastPositionofElementinSortedArray.searchRange(nums,3);//3,4
+        //当target在最后且有重复值
+        //int[] result = problem34FindFirstAndLastPositionofElementinSortedArray.searchRange(nums,7);//10,13
+        //当target在最前且有重复值
+        //int[] result = problem34FindFirstAndLastPositionofElementinSortedArray.searchRange(nums,1);//0,1
+        //当target居中且无重复值
+        //int[] result = problem34FindFirstAndLastPositionofElementinSortedArray.searchRange(nums,4);//5,5
+        //当target不在nums中
+        //int[] result = problem34FindFirstAndLastPositionofElementinSortedArray.searchRange(nums,6);//-1,-1
+
+        /*int[] nums ={1,2,3,4,5,6,7};
+        //当target在最前且无重复值
+        //int[] result = problem34FindFirstAndLastPositionofElementinSortedArray.searchRange(nums,1);
+        //当target在最后且无重复值
+        //int[] result = problem34FindFirstAndLastPositionofElementinSortedArray.searchRange(nums,7);//6,6*/
+
+        /*//当nums内无元素时
+        int[] nums ={};
+        int[] result = problem34FindFirstAndLastPositionofElementinSortedArray.searchRange(nums,7);//-1,-1*/
+
+        //提交用例测试
+        /*int[] nums = {5,7,7,8,8,10};
+        int[] result = problem34FindFirstAndLastPositionofElementinSortedArray.searchRange(nums,6);*/
+
+        /*int[] nums = {0,0,0,1,2,3};
+        int[] result = problem34FindFirstAndLastPositionofElementinSortedArray.searchRange(nums,0);*/
+
         for(int i = 0; i<result.length;++i){
             System.out.print(result[i]+" ");
         }
