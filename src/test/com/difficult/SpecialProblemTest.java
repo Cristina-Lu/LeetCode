5b63956c6d780dd4b6e34cd07b7d9d852458cc7d
Cristina-Lu <luhuiying1024@163.com>
2021-06-10 23:30:30 +0800

    JavaGuide疑问实操记录

--- src/test/com/difficult/SpecialProblemTest.java
+++ src/test/com/difficult/SpecialProblemTest.java
@@ -2,6 +2,8 @@ package com.difficult;
 
 import org.junit.Test;
 
+import java.math.BigDecimal;
+
 public class SpecialProblemTest {
 
     /*
@@ -68,6 +70,46 @@ public class SpecialProblemTest {
         Integer oo = -128;
         Integer pp = -128;
         System.out.println(oo==pp);//true
+
+
+    }
+
+    /*
+    * 浮点数的等值判断
+    * */
+
+    @Test
+    public void equalInFloat(){
+        float a = 1.0f-0.9f;
+        float b = 0.9f-0.8f;
+
+
+        System.out.println(a);//0.100000024
+        System.out.println(b);//0.099999964
+        /*产生不一致的原因是float的精度问题*/
+        System.out.println(a==b);//false
+
+        /*常规比较法，会忽略掉精度*/
+        System.out.println(Math.abs(a-b)<0.00001);//true
+
+        /*转换比较法,这个无法忽略精度问题*/
+        System.out.println(String.valueOf(a).equals(String.valueOf(b)));//false
+
+        /*转为BigDecimal之后限制精度然后用equal*/
+        BigDecimal bigDecimal1 = new BigDecimal(a);
+        BigDecimal bigDecimal2 = new BigDecimal(b);
+        System.out.println(bigDecimal1.equals(bigDecimal2));//false
+        System.out.println(bigDecimal1.compareTo(bigDecimal2)==0);//fales
+
+        /*从开始的时候，a和b就是不同的两个数，所以大多数比较都会得到false的结果
+        * 如果开始两个值就是相同的(包括精度)，可以使用String.valueOf（）转换成String用equal计算
+        * 精度不同值相同，则可以使用String.valueOf()转换成BigDecimal，然后使用compareTo进行比较
+        * */
+
+        BigDecimal bigDecimal3 = new BigDecimal("1.2");
+        BigDecimal bigDecimal4 = new BigDecimal("1.20");
+        System.out.println(bigDecimal3.compareTo(bigDecimal4)==0);//true
+        System.out.println(bigDecimal3.equals(bigDecimal4));//false
 
 
     }
