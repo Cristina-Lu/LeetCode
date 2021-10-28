fde408acf0371e66a00cd79910368620bff1a89d
Cartier Qian <luhuiying1024@163.com>
2021-10-28 19:41:53 +0800

    problem 45 深度优先搜索，尚未成型

---
+++ src/main/com/medium/Problem45JumpGame.java
@@ -0,0 +1,13 @@
+package com.medium;
+
+public class Problem45JumpGame {
+    public int jump(int[] nums) {
+        int jumpTime =0;
+        int afterLength = nums.length-1;
+        for (int i=0;i<nums.length;++i){
+
+        }
+
+        return 0;
+    }
+}
