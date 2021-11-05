de0f066f37c9349449c4e0d5183d5fb43b3dcb0a
Cartier Qian <luhuiying1024@163.com>
2021-11-05 16:30:55 +0800

    problem 53 最大子序和，优化解法

--- src/main/com/simple/Problem53MaximumSubarray.java
+++ src/main/com/simple/Problem53MaximumSubarray.java
@@ -2,7 +2,7 @@ package com.simple;
 
 public class Problem53MaximumSubarray {
     //普通解法
-    public int maxSubArray(int[] nums) {
+    public int maxSubArray1(int[] nums) {
         int[] maxNum = new int[nums.length];
         maxNum[0]= nums[0];
         int Max=nums[0];
@@ -16,6 +16,35 @@ public class Problem53MaximumSubarray {
             if(Max<maxNum[i]){
                 Max = maxNum[i];
             }
+        }
+        return Max;
+    }
+
+    //优化内存
+    public int maxSubArray2(int[] nums) {
+        int maxNum = nums[0];
+        int Max=nums[0];
+        for(int i=1;i<nums.length;++i){
+            int sum =maxNum+nums[i];
+            if(sum>nums[i]){
+                maxNum=sum;
+            }else {
+                maxNum=nums[i];
+            }
+            if(Max<maxNum){
+                Max = maxNum;
+            }
+        }
+        return Max;
+    }
+
+    //二次优化
+    public int maxSubArray(int[] nums) {
+        int maxNum = nums[0];
+        int Max=nums[0];
+        for(int i=1;i<nums.length;++i){
+            maxNum = Math.max(nums[i],maxNum+nums[i]);
+            Max = Math.max(maxNum,Max);
         }
         return Max;
     }
