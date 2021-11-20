6980ac6cab520b3b5221c580ebcd5594681ac480
Cristina-Lu <luhuiying1024@163.com>
2021-11-20 19:36:21 +0800

    problem 61 链表问题，无难度

--- src/main/com/medium/Problem61RotateList.java
+++ src/main/com/medium/Problem61RotateList.java
@@ -4,16 +4,25 @@ import com.toolClasses.ListNode;
 
 public class Problem61RotateList {
     public ListNode rotateRight(ListNode head, int k) {
-        ListNode pro = null;
-        ListNode node = head.next;
+        ListNode pro = head;
+        ListNode node = head;
         ListNode tail = null;
         if(node==null)
             return head;
         int sum = 0;
-        for (int i = 1;node!=null;++i){
-
+        while(node!=null){
+            sum++;
+            if(node.next==null)
+                tail = node;
+            node=node.next;
+        }
+        k=sum-k%sum;
+        for (int i=0;i<k-1;++i){
+            pro = pro.next;
         }
         node = pro.next;
+        if(node == null)
+            return head;
         pro.next = null;
         tail.next = head;
         return node;
