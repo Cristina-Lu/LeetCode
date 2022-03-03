f13298448c96279ec58922ace814a996743e1d88
Cristina-Lu <luhuiying1024@163.com>
2022-03-03 22:03:38 +0800

    滑动窗口内的最大值

--- src/main/com/others/ShopeeProblem1.java
+++ src/main/com/others/ShopeeProblem1.java
@@ -2,18 +2,18 @@ package com.others;
 
 
 public class ShopeeProblem1 {
-    public int[] problem(int[] nums){
+    public int[] problem(int[] nums) {
 
-        int sum[]={0,0,0};
-        for (int i=0;i<nums.length;++i){
+        int sum[] = {0, 0, 0};
+        for (int i = 0; i < nums.length; ++i) {
             sum[nums[i]]++;
         }
-        int j=0;
-        for (int i=0;i<nums.length;++i){
-            while (sum[j]==0){
+        int j = 0;
+        for (int i = 0; i < nums.length; ++i) {
+            while (sum[j] == 0) {
                 ++j;
             }
-            nums[i]=j;
+            nums[i] = j;
             sum[j]--;
         }
         return nums;
